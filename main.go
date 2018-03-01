3ded59b2b35df61cbb97802d265238bc0e7813b4
PC-20170601SNKL\Administrator <pihongbin@idniu.com>
2018-03-01 12:29:10 +0800

    no message

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 //天气查询控制
 func weatherController(w http.ResponseWriter, r *http.Request)  {
     err := r.ParseForm()
-    fmt.Println(r.PostForm)
+    fmt.Println(r)
     if err != nil {
         fmt.Println("数据解析错误")
     }
